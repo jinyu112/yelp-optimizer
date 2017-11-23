ddddb1742ebae565301b97f230c4e648186d8b24
Alice Guan <aliguan726@gmail.com>
2017-11-22 21:05:52 -0500

    Configure router and piping

--- src/index.js
+++ src/index.js
@@ -4,5 +4,13 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+ReactDOM.render(
+    <Router>
+        <div>
+            <Route exact path="/" component={App}/>
+        </div>
+    </Router>
+    , document.getElementById('root'));
 registerServiceWorker();
